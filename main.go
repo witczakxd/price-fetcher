7fafd1f9bc2f112e14366b18bdff666e723d1930
witczakxd <kacperwitczak2003@icloud.com>
2023-11-03 13:40:17 +0100

    x

--- main.go
+++ main.go
@@ -1,23 +1,10 @@
 package main
 
 import (
-	//"context"
 	"flag"
-	//"fmt"
-	//"log"
-
-	//"github.com/witczakxd/price-fetcher/client"
 )
 
 func main() {
-	//client := client.New("http://localhost:3000")
-	//price ,err := client.FetchPrice(context.Background(),"ET")
-	//if err != nil {
-	//	log.Fatal(err)
-	//}
-//
-	//fmt.Printf("%+v\n",price)
-	//return
 	listenAddr := flag.String("listenaddr",":3000","the listen address the service is running")
 	flag.Parse()
 
